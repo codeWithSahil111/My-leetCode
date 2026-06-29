1e40b4378023c156b406330b88f419aa8f569002
Sahil Ansari <codewithsahil619@gmail.com>
2026-06-29 19:06:30 +0530

    Time: 38 ms (76.2%), Space: 127.4 MB (77.04%) - LeetHub

---
+++ 2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<Integer> findLonely(int[] nums) {
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for(int i = 0; i<n; i++){
+             boolean left =
+                    (i == 0) ||
+                    (nums[i] != nums[i - 1] && nums[i] - nums[i - 1] > 1);
+
+            boolean right =
+                    (i == n - 1) ||
+                    (nums[i] != nums[i + 1] && nums[i + 1] - nums[i] > 1);
+
+            if (left && right) {
+                ans.add(nums[i]);
+            }
+        }
+        return ans;
+    }
+}
